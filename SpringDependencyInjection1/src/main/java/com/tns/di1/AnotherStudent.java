b285cd5d78a707489bd72fa8d5e8f78eb7390c26
Niharika Harish <Niharika Harish@DESKTOP-9JACVNH.bbrouter>
2022-09-19 18:11:55 +0530

    First Commit

---
+++ SpringDependencyInjection1/src/main/java/com/tns/di1/AnotherStudent.java
@@ -0,0 +1,14 @@
+package com.tns.di1;
+
+public class AnotherStudent {
+	MathCheat chit;
+
+	public void setChit(MathCheat chit) {
+		this.chit = chit;
+	}
+
+	public void startCheating()
+	 {
+		 chit.mathCheat();
+	 }
+}
---
+++ SpringDependencyInjection1/src/main/java/com/tns/di1/MathCheat.java
@@ -0,0 +1,11 @@
+package com.tns.di1;
+
+public class MathCheat {
+	public MathCheat()
+	 {
+		 System.out.println("Constructor");
+	 }
+		public void mathCheat() {
+			System.out.println("Microxerox is ready");		
+		}
+	}
